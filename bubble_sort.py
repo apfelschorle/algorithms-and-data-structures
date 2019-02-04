1e92a8655232ec3542ba72c7cf975b378a37a242
k.christensen <11416589+kartoffelknoedeln@users.noreply.github.com>
2019-02-04 15:32:11 -0600

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,15 +4,13 @@ def bubble_sort(provided_list):
     while list_length != 0:
         for i in range(len(provided_list) - 1):
             if provided_list[i] > provided_list[i + 1]:
-
                 provided_list[i], provided_list[i + 1] = provided_list[i + 1], provided_list[i]
-                print provided_list
-
         list_length = list_length - 1
+
+    return provided_list
 
 def main():
     provided_list = [4, 6, 7, 1, 5, 3, 0, 2]
-    print 'Sorting step by step:'
-    bubble_sort(provided_list)
+    print(bubble_sort(provided_list))
 
 main()
