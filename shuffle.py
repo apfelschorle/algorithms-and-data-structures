fa6aa56e4eb5addb9ca73b2211662b10610d1749
k.christensen <11416589+kartoffelknoedeln@users.noreply.github.com>
2019-04-10 23:15:04 -0500

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -1,15 +1,15 @@
 import random
 
-def deck_shuffling(shuffle):
-    for i in range(1, len(shuffle)):
-        shuffling = int(random.random() * i)
-        temporary_shuffle = shuffle[i]
-        shuffle[i] = shuffle[shuffling]
+def num_shuffling(shuffle):
+    for index in range(1, len(shuffle)):
+        shuffling = int(random.random() * index)
+        temporary_shuffle = shuffle[index]
+        shuffle[index] = shuffle[shuffling]
         shuffle[shuffling] = temporary_shuffle
     return shuffle
 
 def main():
-    card_deck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print deck_shuffling(card_deck)
+    num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(num_shuffling(num_list))
     
 main()
