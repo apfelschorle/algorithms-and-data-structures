6b3aee73c3477d950d7ae8c89f14deda68be7440
k.christensen <11416589+kartoffelknoedeln@users.noreply.github.com>
2019-04-12 20:19:55 -0500

    Create sampling.py

---
+++ sampling.py
@@ -0,0 +1,21 @@
+import random
+
+def sampling(original_list, sampling_num):
+    sampling_list = []
+    for i in range(sampling_num):
+        sampling_list.append(original_list[i])
+
+    for j in range(sampling_num, len(original_list)):
+        index = int(j * random.random())
+
+        if index < sampling_num:
+            sampling_list[index] = original_list[j]
+    return sampling_list
+            
+
+def main():
+    original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(original_list)
+    print(sampling(original_list, 4))
+
+main()
