7aaff7ebb8868571ef23ed446b78db7267e6e707
kartoffelknoedeln <11416589+kartoffelknoedeln@users.noreply.github.com>
2020-04-04 12:29:40 -0500

    Create stacks.py

---
+++ stacks.py
@@ -0,0 +1,35 @@
+class Stacks:
+    def __init__(self):
+        self.node = []
+
+    def isEmpty(self):
+        return self.node == []
+
+    def peek(self):
+        return self.node
+
+    def push(self, node):
+        return self.node.append(node)
+
+    def pop(self):
+        return self.node.pop()
+
+Stacking = Stacks()
+print('Is the stack empty?')
+print(Stacking.isEmpty())
+print()
+
+print('Adding elements 1, 2, 3, and 4...')
+Stacking.push(1)
+Stacking.push(2)
+Stacking.push(3)
+Stacking.push(4)
+print('... done.')
+print()
+
+print('Is the stack empty?')
+print(Stacking.isEmpty())
+print()
+
+print('What is the stack now?')
+print(Stacking.peek())
